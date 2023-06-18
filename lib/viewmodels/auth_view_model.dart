7344a3132037f3d8c3accb44833c06d6e444d56a
nwj002 <nwj.shrestha@gmail.com>
2023-06-19 00:16:40 +0545

    googlesignin function and upload image option in Add hostel

---
+++ lib/viewmodels/auth_view_model.dart
@@ -0,0 +1,14 @@
+// import '../models/user_model.dart';
+// import '../repositories/user_repository.dart';
+//
+// class AuthViewModel {
+//   final UserRepository _userRepository = UserRepository();
+//
+//   Future<UserModel?> signInWithGoogle() async {
+//     return await _userRepository.signInWithGoogle();
+//   }
+//
+//   Future<void> signOut() async {
+//     await _userRepository.signOut();
+//   }
+// }
--- lib/viewmodels/hostel_view_model.dart
+++ lib/viewmodels/hostel_view_model.dart
@@ -1,11 +1,19 @@
-import '../models/hostel.dart';
+import 'dart:io';
 import '../repositories/hostel_repository.dart';
-
+import '../models/hostel.dart';
 
 class HostelViewModel {
   final HostelRepository _hostelRepository = HostelRepository();
 
-  Future<void> addHostel(Hostel hostel) {
-    return _hostelRepository.addHostel(hostel);
+  Future<void> addHostel(Hostel hostel, File imageFile) async {
+    try {
+      final imageUrl = await _hostelRepository.uploadImage(imageFile);
+      final hostelWithImage = hostel.copyWith(imageUrl: imageUrl);
+      // Save hostel data to Firestore or perform other operations
+      print('Hostel with image: $hostelWithImage');
+    } catch (error) {
+      print('Error adding hostel with image: $error');
+      throw error;
+    }
   }
 }
