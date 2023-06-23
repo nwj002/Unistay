855faf91456a30d911255748580aafd57f3b046c
220049 <220049@softwarica.edu.np>
2023-06-23 17:51:57 +0545

    feature for AddHostel.dart

---
+++ test/feature/step/i_enter_a_valid_hostel_capacity.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> iEnterAValidHostelCapacity(WidgetTester tester) async {
+  throw UnimplementedError();
+}
---
+++ test/feature/step/i_tap_the_save_button.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+Future<void> iTapTheSaveButton(WidgetTester tester) async {
+  throw UnimplementedError();
+}
