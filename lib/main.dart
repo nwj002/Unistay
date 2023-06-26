d99720f2fb76935fc05f14f6d8fee205fca9f97d
Nawaraj shrestha <104550075+nwj002@users.noreply.github.com>
2023-06-26 23:36:50 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
         ),
         initialRoute: "/documents",
+        
         routes: {
           '/login':(context) => SignInScreen(),
           '/splash':(context) => SplashScreen(),
