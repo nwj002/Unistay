b107999f562491e0d7a82baeaa96a2a370962458
madhu502 <chaudharymadhu502@gmail.com>
2023-07-08 16:20:55 +0545

    login screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:unistay/presentation/route/route.dart';
 
 import 'firebase_options.dart';
+import 'logic/provider/user_data_provider.dart';
+import 'logic/services/auth_services/auth_service.dart';
+import 'logic/services/fireStoreServices/user_firestore_service.dart';
 
 
 Future<void> main() async {
   int? initScreen;
 
   WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  initScreen = prefs.getInt("initScreen");
+  await prefs.setInt("initScreen", 1);
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -17,7 +25,19 @@ Future<void> main() async {
   runApp(
     MultiProvider(
       providers: [
-        
+        StreamProvider.value(
+          value: UserDataFirestoreService().getUserData(),
+          initialData: null,),
+
+         ChangeNotifierProvider.value(
+          value: UsereDataProvider(),
+        ),
+        Provider<AuthService>(
+          create: (_) => AuthService(),
+        ), 
+        Provider<UserDataFirestoreService>(
+          create: (_) => UserDataFirestoreService(),
+        ),
       ],
       child: const MyApp(),
     ),
@@ -35,6 +55,7 @@ class MyApp extends StatelessWidget {
         fontFamily: "Brazila",
       ),
       debugShowCheckedModeBanner: false,
+       onGenerateRoute: Routes.generateRoute,
     );
   }
 
