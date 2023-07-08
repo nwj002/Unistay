45b49e3b6c0d2284eb5ecf5b146dee0827f05288
KeharSr <keharojha56@gmail.com>
2023-07-08 19:37:19 +0545

    complaints/raise tickets added

--- lib/core/constant/string.dart
+++ lib/core/constant/string.dart
@@ -3,3 +3,18 @@
 
 //Authentication
 const String logInScreenRoute = '/logInScreenRoute';
+const String registrationScreenRoute = '/registrationScreenRoute';
+
+
+//admin
+const String complaintListScreenRoute = '/AdminComplainListScreen';
+const String approveDenyComplainListRoute ='/AdminApproveDenyComplainListScreen';
+
+
+//student
+const String studentAddComplaintRoute = "/studentAddcomplaintscreen";
+const String electricitycomplaintscreenRoute = "/electricitycomplaint";
+const String studentComplaintListScreenRoute = "/studentComplaintListScreen";
+const String studentDetailScreenRoute = "/studentDetails";
+//drawer
+const String helpscreenRoute = "/help";
