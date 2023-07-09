407371758722e4204f8a88c6a27e2d91165a408a
220049 <220049@softwarica.edu.np>
2023-07-09 20:28:16 +0545

    removed old bdd test of mvvm

--- test/feature/step/i_enter_a_valid_room_name.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-
-Future<void> iEnterAValidRoomName(WidgetTester tester) async {
-  final Finder roomNameField = find.byKey(Key('room_name_field'));
-
-  await tester.enterText(roomNameField, 'Meeting Room');
-}
