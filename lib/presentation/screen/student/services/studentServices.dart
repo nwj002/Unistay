22288733658533a4ced70d474e841686fe7d2c65
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 22:15:28 +0545

    student services

--- lib/presentation/screen/student/services/studentServices.dart
+++ lib/presentation/screen/student/services/studentServices.dart
@@ -31,3 +31,39 @@ class _StudentServicesScreenState extends State<StudentServicesScreen> {
     studentElectricityServiceRoute,
     studentOtherServiceRoute,
   ];
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          backgroundColor: Colors.orangeAccent,
+          title: const Text(
+            'Services',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orangeAccent,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => StudentPastServiceScreen()));
+        },
+        child: Icon(Icons.comment),
+      ),
+      drawer: const StudentDrawer(),
+      body: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          itemCount: images.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 4.0,
+            mainAxisSpacing: 4.0,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, navigatorRoute[index]);
+              },
