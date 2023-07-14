04f9e8e12f021637d8fd91866d00b2839644a910
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 22:14:29 +0545

    student services

---
+++ lib/presentation/screen/student/services/studentServices.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hostelapplication/core/constant/string.dart';
+import 'package:hostelapplication/presentation/screen/student/services/StudentPastService.dart';
+import 'package:hostelapplication/presentation/screen/student/studentDrawer.dart';
+
+class StudentServicesScreen extends StatefulWidget {
+  const StudentServicesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StudentServicesScreen> createState() => _StudentServicesScreenState();
+}
