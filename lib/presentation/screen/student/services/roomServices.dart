5e6e2e97e580b1260d36325829fd8a980c807723
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 21:59:28 +0545

    working on room services

---
+++ lib/presentation/screen/student/services/roomServices.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/repairDeviceWidget.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/serviceTextField.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/userTable.dart';
+import 'package:provider/provider.dart';
+
+import 'package:hostelapplication/logic/modules/userData_model.dart';
+import 'package:hostelapplication/logic/provider/service_provider.dart';
+import 'package:hostelapplication/logic/service/auth_services/auth_service.dart';
+
+class StudentRoomServices extends StatefulWidget {
+  const StudentRoomServices({Key? key}) : super(key: key);
+
+  @override
+  State<StudentRoomServices> createState() => _StudentRoomServicesState();
+}
+class _StudentRoomServicesState extends State<StudentRoomServices> {
+  Map<String, bool> values = {};
+  bool isBedChecked = false;
+  bool isChairChecked = false;
+  bool isDoorChecked = false;
+  List repairDeviceList = [];
