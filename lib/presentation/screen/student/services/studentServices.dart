026b564120cde162adc319a8ea62598c41c17a85
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 22:14:55 +0545

    student services

--- lib/presentation/screen/student/services/studentServices.dart
+++ lib/presentation/screen/student/services/studentServices.dart
@@ -9,3 +9,18 @@ class StudentServicesScreen extends StatefulWidget {
   @override
   State<StudentServicesScreen> createState() => _StudentServicesScreenState();
 }
+class _StudentServicesScreenState extends State<StudentServicesScreen> {
+  List<String> images = [
+    'assets/images/room.png',
+    'assets/images/dinner.png',
+    'assets/images/bath.png',
+    'assets/images/thunderbolt.png',
+    'assets/images/other.png'
+  ];
+  List<String> imagesText = [
+    'Room',
+    'Mess',
+    'Bathroom',
+    'Electricity',
+    'Other'
+  ];
