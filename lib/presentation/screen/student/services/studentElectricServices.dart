383f803c869896e7d393fa113a0b75a9ccbbc57d
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 22:06:27 +0545

    electric service

---
+++ lib/presentation/screen/student/services/studentElectricServices.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/repairDeviceWidget.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/serviceTextField.dart';
+import 'package:hostelapplication/presentation/screen/student/services/widget/userTable.dart';
+import 'package:provider/provider.dart';
+
+import 'package:hostelapplication/logic/modules/userData_model.dart';
+import 'package:hostelapplication/logic/provider/service_provider.dart';
+import 'package:hostelapplication/logic/service/auth_services/auth_service.dart';
