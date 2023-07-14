3cdad166f56852bad1ee0f15985ab093abdba2e4
nwj002 <nwj.shrestha@gmail.com>
2023-07-14 21:59:45 +0545

    working on room services

--- lib/presentation/screen/student/services/roomServices.dart
+++ lib/presentation/screen/student/services/roomServices.dart
@@ -21,3 +21,19 @@ class _StudentRoomServicesState extends State<StudentRoomServices> {
   bool isChairChecked = false;
   bool isDoorChecked = false;
   List repairDeviceList = [];
+   @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    User user = authService.getcurrentUser();
+    final userList = Provider.of<List<UserData>?>(context);
+    Iterable<UserData>? userData =
+        userList?.where((element) => user.uid == element.id);
+    final serviceProvider = Provider.of<ServiceProvider>(context);
+    DateTime now = DateTime.now();
+    const tablepadding = EdgeInsets.all(15);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: const Text("Room Service"),
+      ),
