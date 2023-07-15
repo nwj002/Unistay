ab1cbd331eac0578077ddfa2c1409ba071dd058a
220049 <220049@softwarica.edu.np>
2023-07-15 22:12:03 +0545

    old test deleted

--- test/feature/step/i_enter_a_valid_hostel_name.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:unistay/screens/AddHostel.dart';
-
-Future<void> iEnterAValidHostelName(WidgetTester tester) async {
-  final Finder hostelNameField = find.byKey(Key('hostel_name_field'));
-
-  await tester.enterText(hostelNameField, 'My Hostel');
-}
--- test/feature/step/i_should_see_an_error_message_indicating_a_missing_room_name.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-
-Future<void> iShouldSeeAnErrorMessageIndicatingAMissingRoomName(WidgetTester tester) async {
-  final Finder errorMessage = find.text('Please enter a room name');
-  expect(errorMessage, findsOneWidget);
-}
