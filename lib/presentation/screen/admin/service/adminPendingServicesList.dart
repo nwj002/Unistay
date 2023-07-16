d6691eb5b3bd6c53766402748ff881e21142e561
nwj002 <nwj.shrestha@gmail.com>
2023-07-16 21:55:52 +0545

    Response Ticket

--- lib/presentation/screen/admin/service/adminPendingServicesList.dart
+++ lib/presentation/screen/admin/service/adminPendingServicesList.dart
@@ -6,3 +6,16 @@ class AdminPendingServiceListScreen extends StatelessWidget {
   AdminPendingServiceListScreen(this.serviceTitle);
 
   String serviceTitle;
+
+@override
+  Widget build(BuildContext context) {
+    List<Service> serviceList = [];
+    final serviceListRaw = Provider.of<List<Service>?>(context);
+    serviceListRaw?.forEach(
+      (element) {
+        if (element.status == 0 && serviceTitle == element.serviceTitle) {
+          serviceList.add(element);
+        }
+        ;
+      },
+    );
