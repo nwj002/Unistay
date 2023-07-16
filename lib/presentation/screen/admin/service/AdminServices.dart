a0ad472f1d3ddc6788fb526327d342f877d0330d
nwj002 <nwj.shrestha@gmail.com>
2023-07-16 22:03:54 +0545

    Response Ticket

--- lib/presentation/screen/admin/service/AdminServices.dart
+++ lib/presentation/screen/admin/service/AdminServices.dart
@@ -20,3 +20,28 @@ class _AdminServicesScreenState extends State<AdminServicesScreen> {
     'Electricity',
     'Other'
   ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: const Text(
+          'Services',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: const AdminDrawer(),
+      body: Container(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: GridView.builder(
+                itemCount: images.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0,
+                ),
