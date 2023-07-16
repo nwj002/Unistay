83036872f45f03fe2adc57a28481815c5f76f9ae
nwj002 <nwj.shrestha@gmail.com>
2023-07-16 21:55:15 +0545

    Response Ticket

---
+++ lib/presentation/screen/admin/service/adminPendingServicesList.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+class AdminPendingServiceListScreen extends StatelessWidget {
+  AdminPendingServiceListScreen(this.serviceTitle);
+
+  String serviceTitle;
