c48b139b532d5aec476b6cd98f51ae5c19ae013b
pramesh167 <prameshpathak2002@gmail.com>
2023-07-16 20:58:44 +0545

    furtheraddup privay

--- lib/presentation/screen/student/Drawer/privacypolicy.dart
+++ lib/presentation/screen/student/Drawer/privacypolicy.dart
@@ -22,3 +22,29 @@ class PrivacyPolicyPage extends StatelessWidget {
     'Last updated: July 13, 2023',
     style: TextStyle(fontSize: 12),
     ),
+    SizedBox(height: 16.0),
+    Text(
+    'This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.',
+    ),
+    SizedBox(height: 8.0),
+    Text(
+    'We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Free Privacy Policy Generator.',
+    ),
+    SizedBox(height: 16.0),
+    Text(
+    'Interpretation and Definitions',
+    style: TextStyle(fontWeight: FontWeight.bold),
+    ),
+    SizedBox(height: 8.0),
+    Text('Interpretation'),
+    SizedBox(height: 4.0),
+    Text(
+    'The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.',
+    ),
+    SizedBox(height: 8.0),
+    Text('Definitions', style: TextStyle(fontWeight: FontWeight.bold)),
+    SizedBox(height: 4.0),
+    Text('For the purposes of this Privacy Policy:'),
+    SizedBox(height: 4.0),
+    Text(
+    '- Account means a unique account created for You to access our Service or parts of our Service.'),
