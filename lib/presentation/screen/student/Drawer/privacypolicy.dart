e083b66a8894ab6fa545530df3fbee98009d81c3
pramesh167 <prameshpathak2002@gmail.com>
2023-07-16 21:32:33 +0545

    privacy

--- lib/presentation/screen/student/Drawer/privacypolicy.dart
+++ lib/presentation/screen/student/Drawer/privacypolicy.dart
@@ -111,3 +111,14 @@ class PrivacyPolicyPage extends StatelessWidget {
     Text('- Twitter'),
     Text('- LinkedIn'),
     SizedBox(height: 4.0),
+    Text(
+    'If You decide to register through or otherwise grant us access to a Third-Party Social Media Service, We may collect Personal data that is already associated with Your Third-Party Social Media Service\'s account, such as Your name, Your email address, Your activities or Your contact list associated with that account.'),
+    SizedBox(height: 4.0),
+    Text(
+    'You may also have the option of sharing additional information with the Company through Your Third-Party Social Media Service\'s account. If You choose to provide such information and Personal Data, during registration or otherwise, You are giving the Company permission to use, share, and store it in a manner consistent with this Privacy Policy.'),
+    SizedBox(height: 4.0),
+    Text('Information Collected while Using the Application'),
+    SizedBox(height: 4.0),
+    Text(
+    'While using Our Application, in order to provide features of Our Application, We may collect, with Your prior permission:'),
+    SizedBox(height: 4.0),
