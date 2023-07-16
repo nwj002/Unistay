3b859ddeb35a1a9786d4fafabfd8b3adee537641
nwj002 <nwj.shrestha@gmail.com>
2023-07-16 22:03:41 +0545

    Response Ticket

---
+++ lib/presentation/screen/admin/service/AdminServices.dart
@@ -0,0 +1,22 @@
+class AdminServicesScreen extends StatefulWidget {
+  const AdminServicesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AdminServicesScreen> createState() => _AdminServicesScreenState();
+}
+
+class _AdminServicesScreenState extends State<AdminServicesScreen> {
+  List<String> images = [
+    'assets/images/room.png',
+    'assets/images/dinner.png',
+    'assets/images/bath.png',
+    'assets/images/thunderbolt.png',
+    'assets/images/other.png'
+  ];
+  List<String> imagesText = [
+    'Room',
+    'Mess',
+    'Bathroom',
+    'Electricity',
+    'Other'
+  ];
