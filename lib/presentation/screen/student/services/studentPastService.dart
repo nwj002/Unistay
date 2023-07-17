420cfa27446503c6a0b6f4f2bde0a47447b27068
nwj002 <nwj.shrestha@gmail.com>
2023-07-17 22:20:43 +0545

    past complains

--- lib/presentation/screen/student/services/studentPastService.dart
+++ lib/presentation/screen/student/services/studentPastService.dart
@@ -1,2 +1,7 @@
 class StudentPastServiceScreen extends StatelessWidget {
   StudentPastServiceScreen({Key? key}) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    final authService = Provider.of<AuthService>(context);
+    User user = authService.getcurrentUser();
