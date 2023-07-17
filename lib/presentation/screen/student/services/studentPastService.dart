bf8b85426a8c7360769ad26b6a0e88fbd9d9faa8
nwj002 <nwj.shrestha@gmail.com>
2023-07-17 22:21:38 +0545

    past complains

--- lib/presentation/screen/student/services/studentPastService.dart
+++ lib/presentation/screen/student/services/studentPastService.dart
@@ -5,3 +5,17 @@ class StudentPastServiceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final authService = Provider.of<AuthService>(context);
     User user = authService.getcurrentUser();
+
+ List<Service> serviceList = [];
+    final serviceListRaw = Provider.of<List<Service>?>(context);
+
+    serviceListRaw?.forEach(
+      (element) {
+        print(element);
+        if (user.uid == element.studentUid &&
+            (element.status == 1 || element.status == 2)) {
+          serviceList.add(element);
+        }
+        ;
+      },
+    );
