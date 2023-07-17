b39355b36270eb4ad9384cfc5c2dff04d09d837a
madhu502 <chaudharymadhu502@gmail.com>
2023-07-17 20:40:59 +0545

    visibility effect class created

--- lib/presentation/screen/onBoardingScreen.dart
+++ lib/presentation/screen/onBoardingScreen.dart
@@ -11,3 +11,9 @@ enum AnimationTrigger {
   onActionTrigger,
 }
 abstract class AnimationEffect {}
+
+class VisibilityEffect extends AnimationEffect {
+  final Duration duration;
+
+  VisibilityEffect({required this.duration});
+}
