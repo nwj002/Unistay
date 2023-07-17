e6fba05aec57de1b7d11d40183e608d7a2a6451f
220049 <220049@softwarica.edu.np>
2023-07-17 20:28:31 +0545

    firebase mocks added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:hostelapplication/logic/provider/complaint_provider.dart';
+import 'package:hostelapplication/logic/provider/leave_provider.dart';
+import 'package:hostelapplication/logic/provider/notice_provider.dart';
+import 'package:hostelapplication/logic/provider/service_provider.dart';
+import 'package:hostelapplication/logic/provider/userData_provider.dart';
+import 'package:hostelapplication/logic/service/auth_services/auth_service.dart';
+import 'package:hostelapplication/logic/service/fireStoreServices/complaint_firestore_service.dart';
+import 'package:hostelapplication/logic/service/fireStoreServices/leave_firestore_service.dart';
+import 'package:hostelapplication/logic/service/fireStoreServices/notice_firestore_service.dart';
+import 'package:hostelapplication/logic/service/fireStoreServices/service_firestore_service.dart';
+import 'package:hostelapplication/logic/service/fireStoreServices/user_firestore_services.dart';
+import 'package:hostelapplication/presentation/router/route.dart';
+import 'package:hostelapplication/presentation/screen/onBordingScreen.dart';
+import 'package:hostelapplication/presentation/screen/onboard_model.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:unistay/logic/provider/complaint_provider.dart';
-import 'package:unistay/logic/services/fireStoreServices/complaint_firestore_service.dart';
-import 'package:unistay/presentation/route/route.dart';
-import 'firebase_options.dart';
-import 'logic/provider/user_data_provider.dart';
-import 'logic/services/auth_services/auth_service.dart';
-import 'logic/services/fireStoreServices/user_firestore_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
 
 Future<void> main() async {
   int? initScreen;
@@ -18,57 +26,74 @@ Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   initScreen = prefs.getInt("initScreen");
   await prefs.setInt("initScreen", 1);
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(
-    MultiProvider(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key, this.home}) : super(key: key);
+  String? home;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         StreamProvider.value(
-          value: UserDataFirestoreService().getUserData(),
+          value: ServiceFirestoreService().getService(),
           initialData: null,
         ),
-
+        StreamProvider.value(
+          value: LeaveFirestoreService().getLeave(),
+          initialData: null,
+        ),
+        StreamProvider.value(
+          value: NoticeFirestoreService().getNotice(),
+          initialData: null,
+        ),
         StreamProvider.value(
           value: ComplaintFirestoreService().getComplaintForAdmin(),
           initialData: null,
         ),
-
+        StreamProvider.value(
+          value: UserDataFirestoreService().getUserData(),
+          initialData: null,
+        ),
+        ChangeNotifierProvider.value(
+          value: NoticeProvider(),
+        ),
         ChangeNotifierProvider.value(
           value: ComplaintProvider(),
         ),
-
-
         ChangeNotifierProvider.value(
           value: UsereDataProvider(),
         ),
+        ChangeNotifierProvider.value(
+          value: LeaveProvider(),
+        ),
+        ChangeNotifierProvider.value(
+          value: ServiceProvider(),
+        ),
         Provider<AuthService>(
           create: (_) => AuthService(),
-        ), 
+        ),
         Provider<UserDataFirestoreService>(
           create: (_) => UserDataFirestoreService(),
         ),
+        Provider(create: (_) => OnboardingScreen()),
+        Provider(create: (_) => SplashModel())
       ],
-      child: const MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Color.fromARGB(255, 232, 159, 49),
-        fontFamily: "Brazila",
+      child: MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.orangeAccent,
+          fontFamily: "Brazila",
+        ),
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: (route) => Routes.generateRoute(route, home),
       ),
-      debugShowCheckedModeBanner: false,
-       onGenerateRoute: Routes.generateRoute,
     );
   }
-
 }
