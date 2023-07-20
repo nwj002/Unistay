aa0087bd4b6dbb78fa26805bd0407a4255a46b6f
nwj002 <nwj.shrestha@gmail.com>
2023-07-20 21:43:47 +0545

    appeal services

--- lib/presentation/screen/student/Drawer/myservices.dart
+++ lib/presentation/screen/student/Drawer/myservices.dart
@@ -1,2 +1,6 @@
 class Myservicesrequest extends StatelessWidget {
   const Myservicesrequest({Key? key}) : super(key: key);
+ @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    List<Service> servicesList = [];
