abbccab3d0a8890694900fa72ae2988ab68b4feb
220049 <220049@softwarica.edu.np>
2023-07-21 18:27:08 +0545

    approve deny test

---
+++ test/admin/approveDenyCompScreen.dart_widgetTest.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_storage_mocks/firebase_storage_mocks.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unistay/logic/modules/complaint_model.dart';
+import 'package:unistay/logic/provider/complaint_provider.dart';
+import 'package:unistay/logic/service/firebase_service.dart';
+import 'package:unistay/presentation/screen/admin/complaint/approveDenyCompScreen.dart';
+import 'dart:io';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    // setup mock firebase core
+    setupFirebaseCoreMocks();
+    // dummy config for firebase
+    await Firebase.initializeApp(
+      name: 'test',
+      options: const FirebaseOptions(
+        apiKey: 'test',
+        appId: 'test',
+        messagingSenderId: 'test',
+        projectId: 'test',
+      ),
+    );
+    // mock instances
+    final auth = MockFirebaseAuth();
+    final firestore = FakeFirebaseFirestore();
+    final storage = MockFirebaseStorage();
+
+    // set firebase service to mock instances
+    FirebaseService.db = firestore;
+    FirebaseService.auth = auth;
+    FirebaseService.storage = storage.ref();
+
+    // network/http fix
+    HttpOverrides.global = null;
+  });
+
+
+    testWidgets('Approve/Deny Complaint List Test', (WidgetTester tester) async {
+    final complaintProvider = ComplaintProvider();
+
+    await tester.pumpWidget(
+      ChangeNotifierProvider<ComplaintProvider>.value(
+        value: complaintProvider,
+        child: MaterialApp(
+          home: ApproveDenyComplainList(
+            Complaint(
+              name: 'John Doe',
+              roomNo: '123',
+              time: DateTime.now(),
+              complaintTitle: 'Water Issue',
+              complaint: 'There is a leak in the bathroom.',
+              studentUid: '212',
+              status: 1,
+              id: '1',
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Check if the app bar title is displayed correctly
+    expect(find.text('Approve/Denny Complaint'), findsOneWidget);
+
+    // Check if the complaint details are displayed correctly
+    expect(find.text('John Doe'), findsOneWidget);
+    expect(find.text('Room No.'), findsOneWidget);
+    expect(find.text('Water Issue'), findsOneWidget);
+    expect(find.text('There is a leak in the bathroom.'), findsOneWidget);
+
+    // Tap on the "Deny" button and check if the complaint status is changed to 2
+    await tester.tap(find.text('Denny'));
+    await tester.pumpAndSettle();
+    expect(complaintProvider.getCommplaint, 2);
+
+    // Go back to the previous screen
+    Navigator.pop(tester.element(find.byType(ApproveDenyComplainList)));
+    await tester.pumpAndSettle();
+
+    // Tap on the "Approve" button and check if the complaint status is changed to 1
+    await tester.tap(find.text('Approve'));
+    await tester.pumpAndSettle();
+    expect(complaintProvider.getCommplaint, 1);
+  });
+}
