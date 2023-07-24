f6c808be3c32b368a6ad37dcc578022ce925ea14
KeharSr <keharojha56@gmail.com>
2023-07-24 19:13:58 +0545

    disciplinary act rule no 3 added

--- lib/presentation/screen/student/Drawer/diciplinary_act.dart
+++ lib/presentation/screen/student/Drawer/diciplinary_act.dart
@@ -18,11 +18,14 @@ class _DisciplinaryState extends State<Disciplinary> {
     body: SingleChildScrollView(
     child: Column(
     children: [
-    ListTile(
-    title: Text('(Rule 1)  Respect for Others: \n a. Treat fellow residents, staff, and visitors with respect, courtesy, and dignity. \n b. Avoid engaging in any form of harassment, bullying, or discrimination based on race, gender, religion, nationality, or any other characteristic. \n c. Maintain a peaceful and harmonious living environment for all residents. '),
+      ListTile(
+        title: Text('(Rule 1)  Respect for Others: \n a. Treat fellow residents, staff, and visitors with respect, courtesy, and dignity. \n b. Avoid engaging in any form of harassment, bullying, or discrimination based on race, gender, religion, nationality, or any other characteristic. \n c. Maintain a peaceful and harmonious living environment for all residents. '),
     ),
       ListTile(
         title: Text('(Rule 2) Quiet Hours: \n a. Observe designated quiet hours during which noise levels should be minimized to ensure a conducive environment for studying and resting. \n b. Respect the rights of other residents by refraining from excessive noise and disruptive behavior at all times.'),
+      ),
+      ListTile(
+        title: Text('(Rule 3)  Guest Policy: \n a. Adhere to the hostels guest policy, which may include limitations on the number of guests allowed, visitation hours, and registration procedures.\n  b. Ensure that your guests comply with the hostels rules and regulations during their visit. '),
       ),
 
     ],
