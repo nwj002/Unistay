57eed25cda51a83a26d628ca7e7fa4ef2c3ec472
KeharSr <keharojha56@gmail.com>
2023-07-24 19:12:21 +0545

    disciplinary act rule 2 added

--- lib/presentation/screen/student/Drawer/diciplinary_act.dart
+++ lib/presentation/screen/student/Drawer/diciplinary_act.dart
@@ -21,6 +21,10 @@ class _DisciplinaryState extends State<Disciplinary> {
     ListTile(
     title: Text('(Rule 1)  Respect for Others: \n a. Treat fellow residents, staff, and visitors with respect, courtesy, and dignity. \n b. Avoid engaging in any form of harassment, bullying, or discrimination based on race, gender, religion, nationality, or any other characteristic. \n c. Maintain a peaceful and harmonious living environment for all residents. '),
     ),
+      ListTile(
+        title: Text('(Rule 2) Quiet Hours: \n a. Observe designated quiet hours during which noise levels should be minimized to ensure a conducive environment for studying and resting. \n b. Respect the rights of other residents by refraining from excessive noise and disruptive behavior at all times.'),
+      ),
+
     ],
     ),
     )
