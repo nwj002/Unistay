ab9575c6ffbdaaa72f1fa534cc7bd49ff7257bc7
smritikhagdi98 <smritikhagi07@gmail.com>
2023-07-25 13:07:02 +0545

    working on view leaves

--- lib/presentation/screen/student/Drawer/myleave.dart
+++ lib/presentation/screen/student/Drawer/myleave.dart
@@ -32,26 +32,44 @@ class MyLeave extends StatelessWidget {
     body: Leavelist != []
     ? Padding(
     padding: EdgeInsets.all(8),
-    child: ListView.builder(
-    itemCount: Leavelist.length,
-    itemBuilder: (context, index){
-    return MyLeaveListModel(
-    leavingdate: Leavelist[index].dateOfLeave,
-    commingdate: Leavelist[index].dateOfComing,
-    leavereason: Leavelist[index].leaveReason,
-    totalday: Leavelist[index].totalDay,
-    deleteleave: () {
-    showDialog(
-    context: context,
-    builder: (_) => AlertDialog(
-    content: Text("Are you sure you want to delete ?"),
-    actions: [
-      TextButton(
-    child: Text(
-    "Cancel",
-    style: TextStyle(color: Colors.black),
-    ),
-    onPressed: () {
-    Navigator.of(context).pop();
-    },
-    ),
+          child: ListView.builder(
+          itemCount: Leavelist.length,
+          itemBuilder: (context, index){
+          return MyLeaveListModel(
+              leavingdate: Leavelist[index].dateOfLeave,
+              commingdate: Leavelist[index].dateOfComing,
+              leavereason: Leavelist[index].leaveReason,
+              totalday: Leavelist[index].totalDay,
+              deleteleave: () {
+                  showDialog(
+                  context: context,
+                  builder: (_) => AlertDialog(
+                  content: Text("Are you sure you want to delete ?"),
+                  actions: [
+                    TextButton(
+                  child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {
+                        Navigator.of(context).pop();
+                        },
+                        ),
+                        TextButton(
+                            child: Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.red),
+                            ),
+                                  onPressed: () {
+                                  leaveprovider.deleteLeave(Leavelist[index].id);
+                                  Navigator.of(context).pop();
+                                  },
+                                  ),
+                                ],
+                              ),
+                              );
+                                  },
+                           );
+                         },
+                      ),
+                       )
