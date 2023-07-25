295b3e8f9ca1ce5e8923d0c2e0a8b35eb0653163
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 14:15:45 +0545

    boxdecoration

--- lib/presentation/screen/admin/admindetilaspage/admindetails.dart
+++ lib/presentation/screen/admin/admindetilaspage/admindetails.dart
@@ -24,3 +24,9 @@ class AdminDetailsScreen extends StatelessWidget {
     const SizedBox(
     height: 30,
     ),
+    Container(
+    padding: const EdgeInsets.all(10.0),
+    width: MediaQuery.of(context).size.width / 3,
+    height: MediaQuery.of(context).size.width / 3,
+    decoration: BoxDecoration(
+    border: Border.all(color: Colors.black, width: 1),
