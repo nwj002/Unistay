5d960766ece9a92c7ab45d53a415417333f615d9
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 12:18:42 +0545

    help2

--- lib/presentation/screen/student/Drawer/helpscreen.dart
+++ lib/presentation/screen/student/Drawer/helpscreen.dart
@@ -3,3 +3,10 @@ import 'package:flutter/material.dart';
 
 class HelpScreen extends StatelessWidget {
   const HelpScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final helpstyle = TextStyle(
+        color: Colors.blue.shade900, fontWeight: FontWeight.bold, fontSize: 20);
+    return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
