e1783b3f357a05918591bba59c0c5c68d365b6e7
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 14:14:17 +0545

    widgs

--- lib/presentation/screen/admin/admindetilaspage/admindetails.dart
+++ lib/presentation/screen/admin/admindetilaspage/admindetails.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 class AdminDetailsScreen extends StatelessWidget {
   const AdminDetailsScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    const String studentname = "Admin";
