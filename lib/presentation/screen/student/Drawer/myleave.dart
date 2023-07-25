83ab84ab6555795698968f37343a2df6c81b6cf8
smritikhagdi98 <smritikhagi07@gmail.com>
2023-07-25 12:59:45 +0545

    working on view leaves

--- lib/presentation/screen/student/Drawer/myleave.dart
+++ lib/presentation/screen/student/Drawer/myleave.dart
@@ -1,1 +1,21 @@
-//here you go with leaves
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:hostelapplication/logic/modules/leave_model.dart';
+import 'package:hostelapplication/logic/provider/leave_provider.dart';
+import 'package:provider/provider.dart';
+
+class MyLeave extends StatelessWidget {
+  const MyLeave({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    List<Leave> Leavelist = [];
+    final leaveprovider = Provider.of<LeaveProvider>(context);
+    final leaveListRaw = Provider.of<List<Leave>?>(context);
+    leaveListRaw?.forEach((element) {
+      if (auth.currentUser?.uid == element.studentUid && element.status == 0) {
+        Leavelist.add(element);
+      }
+      ;
+    });
