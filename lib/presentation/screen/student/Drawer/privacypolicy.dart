c408832b132fb014dfbfeeffe96c3a02171c5d09
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 12:45:19 +0545

    ppdrawer

--- lib/presentation/screen/student/Drawer/privacypolicy.dart
+++ lib/presentation/screen/student/Drawer/privacypolicy.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
-
+class PrivacyPolicyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Privacy Policy'),
