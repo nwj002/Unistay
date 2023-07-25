6685464693e216be28003aebdfb560cc17f1d14d
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 12:48:28 +0545

    allthepolicies

--- lib/presentation/screen/student/Drawer/privacypolicy.dart
+++ lib/presentation/screen/student/Drawer/privacypolicy.dart
@@ -21,3 +21,12 @@ class PrivacyPolicyPage extends StatelessWidget {
     Text(
     'Last updated: July 13, 2023',
     style: TextStyle(fontSize: 12),
+    ),
+    SizedBox(height: 16.0),
+    Text(
+    'This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.',
+    ),
+    SizedBox(height: 8.0),
+    Text(
+    'We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the Free Privacy Policy Generator.',
+    ),
