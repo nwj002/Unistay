a4d040aa7775817fe1c85355fc2e337811557a94
pramesh167 <prameshpathak2002@gmail.com>
2023-07-25 12:47:02 +0545

    sizedboxupdations

--- lib/presentation/screen/student/Drawer/privacypolicy.dart
+++ lib/presentation/screen/student/Drawer/privacypolicy.dart
@@ -12,3 +12,12 @@ class PrivacyPolicyPage extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
     child: Column(
     crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+    Text(
+    'Privacy Policy',
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    SizedBox(height: 8.0),
+    Text(
+    'Last updated: July 13, 2023',
+    style: TextStyle(fontSize: 12),
