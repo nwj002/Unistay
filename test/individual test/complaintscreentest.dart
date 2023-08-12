120c98ede53b202faddfa13aa42ec4d38da309a5
KeharSr <keharojha56@gmail.com>
2023-08-12 13:11:04 +0545

    testing

--- test/individual test/complaintscreentest.dart
+++ test/individual test/complaintscreentest.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:unistay/presentation/screen/admin/complaint/admin_complaint_screen.dart';
+import 'package:unistay/presentation/screen/admin/complaint/decline_approve_list_screen.dart';
 
 void main() {
   testWidgets('AdminComplaintScreen widget test', (WidgetTester tester) async {
@@ -26,17 +27,17 @@ void main() {
     expect(find.text('Declined Complaints'), findsOneWidget);
   });
 
-//   testWidgets('Navigation test for Apporoved Complaints',
-//           (WidgetTester tester) async {
-//         await tester.pumpWidget(MaterialApp(home: AdminComplaintScreen()));
-//
-//         // Tap the 'Approved Complaints' button
-//         await tester.tap(find.text('Apporoved Complaints'));
-//         await tester.pumpAndSettle();
-//
-//         // Verify that the navigation occurred and the correct screen is displayed
-//         expect(find.byType(DeclineApproveListScreen), findsOneWidget);
-//       });
+  testWidgets('Navigation test for Apporoved Complaints',
+          (WidgetTester tester) async {
+        await tester.pumpWidget(MaterialApp(home: AdminComplaintScreen()));
+
+        // Tap the 'Approved Complaints' button
+        await tester.tap(find.text('Apporoved Complaints'));
+        await tester.pumpAndSettle();
+
+        // Verify that the navigation occurred and the correct screen is displayed
+        expect(find.byType(DeclineApproveListScreen), findsOneWidget);
+      });
 //
 //   testWidgets('Navigation test for Declined Complaints',
 //           (WidgetTester tester) async {
